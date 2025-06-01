Fixed missing gradient in regularization term

mini_trainer/classifier.py
@@ -147,4 +147,4 @@ def last_layer_weights(model : nn.Module):
         return None
     elif not isinstance(classification_head, Classifier):
         raise RuntimeError(f"Unexpected classification head type {type(classification_head)} found.")
-    return classification_head.linear._parameters["weight"].data
+    return classification_head.linear.weight

mini_trainer/utils/plot.py
@@ -282,7 +282,7 @@ def plot_heatmap(
     return fig, ax
 
 def class_distance(classification_weights : torch.Tensor, probability : bool=True):
-    classification_weights = classification_weights.cpu().clone().to(torch.float64)
+    classification_weights = classification_weights.cpu().clone().detach().to(torch.float64)
     classification_weights -= classification_weights.mean(dim = 1, keepdim=True)
     classification_weights /= classification_weights.std(dim = 1, unbiased=True, keepdim=True)
 