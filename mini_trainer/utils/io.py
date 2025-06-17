4b89a65a39a8ed39c56f4c811bdba1548ef130bf
asgersvenning <asgersvenning@gmail.com>
2025-06-17 16:25:55 +0200

    Fixed spelling mistake

--- mini_trainer/utils/io.py
+++ mini_trainer/utils/io.py
@@ -99,7 +99,7 @@ class LazyDataset(torch.utils.data.Dataset):
             return
 
         # 1. Process all items into a list
-        processed_items = thread_map(self.func, self.items, tqmd_class=TQDM, desc="Caching dataset in RAM...", leave=False)
+        processed_items = thread_map(self.func, self.items, tqdm_class=TQDM, desc="Caching dataset in RAM...", leave=False)
         first_item = processed_items[0]
 
         # 2. Check if the function returns a single tensor or a sequence
